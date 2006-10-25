e534c20c1a00c5ae2388053dc6e2751e64295690
Joerg Hohwiller <joerg@j-hohwiller.de>
2006-10-25 20:14:49 +0000

    refactoring utils

---
+++ mmm-util/mmm-util-misc/src/test/java/net/sf/mmm/util/NumericUtilTest.java
@@ -0,0 +1,45 @@
+/* $Id$ */
+package net.sf.mmm.util;
+
+import junit.framework.TestCase;
+
+
+/**
+ * This is the test-case for {@link net.sf.mmm.util.NumericUtil}.
+ *
+ * @author Joerg Hohwiller (hohwille at users.sourceforge.net)
+ */
+@SuppressWarnings("all")
+public class NumericUtilTest extends TestCase {
+
+    /**
+     * The constructor.
+     */
+    public NumericUtilTest() {
+
+        super();
+    }
+
+    private void verifySimplestNumber(Number value, Class simplestType) {
+        Number simpleValue = NumericUtil.toSimplestNumber(value);
+        assertEquals(simpleValue.doubleValue(), value.doubleValue(), 0.0);
+        assertSame(simpleValue.getClass(), simplestType);
+    }
+    
+    /**
+     * 
+     *
+     */
+    public void testToSimplestNumber() {
+        verifySimplestNumber(new Double(1.0), Byte.class);
+        verifySimplestNumber(new Float(128.0), Short.class);
+        verifySimplestNumber(new Double(1234567L), Integer.class);
+        verifySimplestNumber(new Double(12345678901234576L), Long.class);
+        verifySimplestNumber(new Double(42.25), Float.class);
+        verifySimplestNumber(new Double(42.2F), Float.class);
+        verifySimplestNumber(new Double(42.2), Double.class);
+        //verifySimplestNumber(Double.parseDouble("42.2F"), Float.class);
+        verifySimplestNumber(new Double(42.4242424242), Double.class);
+    }
+    
+}
