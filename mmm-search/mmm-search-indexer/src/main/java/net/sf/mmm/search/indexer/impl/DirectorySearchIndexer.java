696e7e16c2b42c786081cfbf646352fcb41c6bc6
Joerg Hohwiller <joerg@j-hohwiller.de>
2007-05-03 18:03:18 +0000

    improved

--- mmm-search/mmm-search-indexer/src/main/java/net/sf/mmm/search/indexer/impl/DirectorySearchIndexer.java
+++ mmm-search/mmm-search-indexer/src/main/java/net/sf/mmm/search/indexer/impl/DirectorySearchIndexer.java
@@ -20,6 +20,7 @@ import javax.annotation.Resource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.impl.NoOpLog;
 
+import net.sf.mmm.search.api.SearchEntry;
 import net.sf.mmm.search.indexer.api.MutableSearchEntry;
 import net.sf.mmm.search.indexer.api.SearchIndexer;
 import net.sf.mmm.search.parser.api.ContentParser;
@@ -28,6 +29,7 @@ import net.sf.mmm.search.parser.impl.ContentParserServiceImpl;
 import net.sf.mmm.util.filter.FileFilterAdapter;
 import net.sf.mmm.util.filter.Filter;
 import net.sf.mmm.util.filter.FilterRuleChainParser;
+import net.sf.mmm.util.io.FileUtil;
 
 /**
  * TODO: this class ...
@@ -172,28 +174,59 @@ public class DirectorySearchIndexer {
     }
   }
 
-  protected String getExtension(String filename) {
-
-    int lastDot = filename.lastIndexOf('.');
-    String extension;
-    if (lastDot > 0) {
-      extension = filename.substring(lastDot + 1).toLowerCase();
-    } else {
-      extension = "";
-    }
-    return extension;
-  }
-
+  /**
+   * This method starts the indexing from the given <code>directory</code>.
+   * 
+   * @see #indexDirectory(String, File)
+   * 
+   * @param directory
+   *        is the directory to index recursively.
+   */
   public void indexDirectory(File directory) {
 
     indexDirectory(null, directory);
   }
 
+  /**
+   * This method starts the indexing from the given <code>directory</code>
+   * adding the given <code>source</code> as metadata.
+   * 
+   * @param source
+   *        is the {@link SearchEntry#getSource() source} attribute of the
+   *        indexed entries.
+   * @param directory
+   *        is the directory to index recursively.
+   */
   public void indexDirectory(String source, File directory) {
 
     indexDirectory(source, directory, "");
   }
 
+  /**
+   * This method starts the indexing from the given <code>directory</code>
+   * adding the given <code>source</code> as metadata.
+   * 
+   * @see SearchEntry#getSource()
+   * 
+   * @param source
+   *        is the {@link SearchEntry#getSource() source} attribute of the
+   *        indexed entries.
+   * @param directory
+   *        is the directory to index recursively.
+   * @param relativePath
+   *        is the base path where used to build the
+   *        {@link SearchEntry#getUri() URI} of the indexed entries. E.g. when
+   *        <code>source</code> is <code>"svn"</code> and
+   *        <code>directory</code> points to where you checked out a
+   *        subversion repository then <code>relativePath</code> may be
+   *        <code>"trunk"</code>. This is especially useful, when you index
+   *        multiple sub-directories from the same <code>source</code>. You
+   *        could also use <code>"http://svn.foo.bar/trunk"</code> as
+   *        <code>relativePath</code> but this would cause a lot of
+   *        unnecessary redundancies in your index. Besides you would rather
+   *        search for <code>source:svn</code> than
+   *        <code>uri:http://svn.foo.bar/trunk/*</code>.
+   */
   public void indexDirectory(String source, File directory, String relativePath) {
 
     for (File child : directory.listFiles()) {
@@ -209,6 +242,18 @@ public class DirectorySearchIndexer {
     }
   }
 
+  /**
+   * This method gets the property <code>key</code> from the given
+   * <code>properties</code>. It will also {@link String#trim() trim} the
+   * properties value.
+   * 
+   * @param properties
+   *        is where to get the property from.
+   * @param key
+   *        is the name of the requested property.
+   * @return the trimmed property or <code>null</code> if the property is NOT
+   *         set or its trimmed value is the empty string.
+   */
   protected String getProperty(Properties properties, String key) {
 
     String value = properties.getProperty(key);
@@ -221,13 +266,26 @@ public class DirectorySearchIndexer {
     return value;
   }
 
+  /**
+   * This method indexes a single file.
+   * 
+   * @param source
+   *        is the {@link SearchEntry#getSource() source} attribute of the
+   *        indexed entry.
+   * @param file
+   *        is the file to index.
+   * @param relativePath
+   *        is the path of the folder where the file is located relative to the
+   *        path given when the indexing was started. This is used to build the
+   *        {@link SearchEntry#getUri() URI} of the file in the search index.
+   */
   public void indexFile(String source, File file, String relativePath) {
 
     this.logger.debug("Indexing " + file.getPath());
     String filename = file.getName();
     String fullPath = relativePath + "/" + filename;
     long fileSize = file.length();
-    String extension = getExtension(filename);
+    String extension = FileUtil.getExtension(filename);
     MutableSearchEntry entry = this.indexer.createEntry();
     entry.setUri(fullPath);
     entry.setSize(fileSize);
