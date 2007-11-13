707d29b61936f60eaaae2cf38229a747726a9f52
Joerg Hohwiller <joerg@j-hohwiller.de>
2007-11-13 21:29:24 +0000

    pojo -> reflect

--- mmm-util/mmm-util-reflect/src/main/java/net/sf/mmm/util/reflect/package-info.java
+++ mmm-util/mmm-util-reflect/src/main/java/net/sf/mmm/util/reflect/package-info.java
@@ -3,15 +3,15 @@
  * http://www.apache.org/licenses/LICENSE-2.0 */
 /**
  * Provides utilities for dealing with reflection.
- * <h2>mmm-util-reflect</h2>
+ * <h2>Reflection Utilities</h2>
  * Java reflection is a powerful mechanism that allows flexible and generic 
  * programming. However the reflection API is NOT always easy for instance when 
  * dealing with generics.<br>
  * This project provides utilities that make reflection a lot easier. It also
  * offers features that you may not even know they where possible such as 
  * finding all classes located in the classpath or a specific package.<br>
- * Therefore {@link net.sf.mmm.util.reflect.ReflectionUtil} will make your life 
- * easier when dealing with reflection. For advanced support when reading 
- * annotations have a look at {@link net.sf.mmm.util.reflect.AnnotationUtil}.
+ * Therefore {@link net.sf.mmm.util.reflect.ReflectionUtil} will help you to 
+ * deal with reflection. For advanced support when reading annotations have a 
+ * look at {@link net.sf.mmm.util.reflect.AnnotationUtil}.
  */
 package net.sf.mmm.util.reflect;
