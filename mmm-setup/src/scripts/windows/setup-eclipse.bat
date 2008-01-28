366f7220ca5b19f1027c952fb64604fc940f64f1
Joerg Hohwiller <joerg@j-hohwiller.de>
2008-01-28 21:44:07 +0000

    1871581: setup script does not work properly applied patches from Marc

--- mmm-setup/src/scripts/windows/setup-eclipse.bat
+++ mmm-setup/src/scripts/windows/setup-eclipse.bat
@@ -2,10 +2,10 @@ rem $Id$
 
 cd %~dp0
 call include.bat
-if exist %ECLIPSE_WORKSPACE% (
+if exist "%ECLIPSE_WORKSPACE%" (
   echo Setting up eclipse projects...
   cd %MMM_BASE%
-  call mvn -Declipse.workspace=%ECLIPSE_WORKSPACE% eclipse:add-maven-repo 
+  call mvn "-Declipse.workspace=%ECLIPSE_WORKSPACE%" eclipse:add-maven-repo 
   call mvn eclipse:eclipse && echo Eclipse successfully setup for mmm.
 ) else (
   echo Eclipse workspace does NOT exist. Please launch eclipse once before
