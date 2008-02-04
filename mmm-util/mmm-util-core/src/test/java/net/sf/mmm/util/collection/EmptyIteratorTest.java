4ff6bedfb0eb5d2588b2975ebf3e52c8f530aeca
Joerg Hohwiller <joerg@j-hohwiller.de>
2008-02-04 20:10:30 +0000

    improve test-coverage

---
+++ mmm-util/mmm-util-core/src/test/java/net/sf/mmm/util/collection/EmptyIteratorTest.java
@@ -0,0 +1,40 @@
+/* $Id$
+ * Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+package net.sf.mmm.util.collection;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+
+/**
+ * This is the test-case for {@link EmptyIterator}.
+ * 
+ * @author Joerg Hohwiller (hohwille at users.sourceforge.net)
+ */
+@SuppressWarnings("all")
+public class EmptyIteratorTest {
+
+  @Test
+  public void testEmpty() {
+
+    Iterator<String> it = EmptyIterator.getInstance();
+    assertFalse(it.hasNext());
+    try {
+      it.next();
+      fail();
+    } catch (NoSuchElementException e) {
+    }
+    try {
+      it.remove();
+      fail();
+    } catch (IllegalStateException e) {
+    } catch (UnsupportedOperationException e) {
+    }
+  }
+
+}
