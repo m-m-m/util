003b87c9d882ebf360404f0f9e0e7d13b640c962
Joerg Hohwiller <joerg@j-hohwiller.de>
2008-02-20 21:18:50 +0000

    improved

--- mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/collection/package-info.java
+++ mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/collection/package-info.java
@@ -4,7 +4,17 @@
 /**
  * Contains utilities for dealing with {@link java.util.Collections}.
  * <h2>Collection Utilities</h2>
- * TODO
+ * This package contains interfaces for factories (especially 
+ * {@link net.sf.mmm.util.collection.CollectionFactory} and 
+ * {@link net.sf.mmm.util.collection.MapFactory}) of the various 
+ * containers offered by the JDK (see {@link java.util}). For each factory 
+ * there is a default implementation accessible via the interface.<br>
+ * All factories are bundled together via the 
+ * {@link net.sf.mmm.util.collection.CollectionFactoryManager} that allows 
+ * to create instances of {@link java.util.Collections} and 
+ * {@link java.util.Map}s in a generic way.<br>
+ * Finally the {@link net.sf.mmm.util.collection.AbstractIterator} makes it 
+ * easier to implement read-only custom {@link java.util.Iterator}s.
  */
 package net.sf.mmm.util.collection;
 
