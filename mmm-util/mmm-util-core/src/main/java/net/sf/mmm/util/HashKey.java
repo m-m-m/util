bd4a1fe12a03fc50882ae3c0c2163b3874cf7acf
Joerg Hohwiller <joerg@j-hohwiller.de>
2008-02-27 21:20:13 +0000

    moved

--- mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/HashKey.java
+++ mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/HashKey.java
@@ -1,7 +1,7 @@
 /* $Id$
  * Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0 */
-package net.sf.mmm.util.collection;
+package net.sf.mmm.util;
 
 /**
  * This is a simple class that allows to use any {@link #getDelegate() object}
