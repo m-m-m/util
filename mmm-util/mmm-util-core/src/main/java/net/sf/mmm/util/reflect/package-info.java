f2cc8289a94914e5e95a37ad13bfa616c081e0cd
Joerg Hohwiller <joerg@j-hohwiller.de>
2008-05-05 20:22:52 +0000

    doc improved

--- mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/reflect/package-info.java
+++ mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/reflect/package-info.java
@@ -5,8 +5,25 @@
  * Provides utilities for dealing with reflection.
  * <h2>Reflection Utilities</h2>
  * Java reflection is a powerful mechanism that allows flexible and generic 
- * programming. However the reflection API is NOT always easy for instance when 
- * dealing with generics.<br>
+ * programming. With Java5 a generic type-system was introduced. However this
+ * is quite a wild animal and therefore hard to deal with via the reflection 
+ * API. Have a look at the following example:
+ * <verbatim>
+ * public class Foo<A, B> {
+ *   A getA() { ... }
+ *   B getB() { ... }
+ * }
+ * public class Bar<X> extends Foo<X, String> {
+ *   ...
+ * }
+ * public class Some extends Bar<Long> {
+ *   ...
+ * } 
+ * </verbatim>
+ * If you want to determine the type of <code>Some.getA()</code> reflectively, 
+ * you will have to dive into the deepest and trickiest part of the reflection
+ * API and might step into one of the many pitfalls on this way. Or you simply
+ * use this util.<br>
  * This project provides utilities that make reflection a lot easier. It also
  * offers features that you may not even know they where possible such as 
  * finding all classes located in the classpath or a specific package.<br>
