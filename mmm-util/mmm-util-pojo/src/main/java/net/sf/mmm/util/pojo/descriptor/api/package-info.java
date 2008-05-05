5a2f62e133989e39c0cf04da41bca83dc0cf3366
Joerg Hohwiller <joerg@j-hohwiller.de>
2008-05-05 20:31:35 +0000

    doc improved

--- mmm-util/mmm-util-pojo/src/main/java/net/sf/mmm/util/pojo/descriptor/api/package-info.java
+++ mmm-util/mmm-util-pojo/src/main/java/net/sf/mmm/util/pojo/descriptor/api/package-info.java
@@ -7,10 +7,11 @@
  * <h2>POJO Introspection API</h2>
  * This package provides the 
  * {@link net.sf.mmm.util.pojo.descriptor.api.PojoDescriptorBuilder}.
- * Unlike the {@link java.beans.Introspector} for java-beans or
+ * Unlike the {@link java.beans.Introspector} for Java-beans or
  * <a href="http://commons.apache.org/beanutils">commons-beanutils</a>,
  * it works on any {@link net.sf.mmm.util.pojo.api.Pojo} and is NOT 
- * limited by restrictions of the java-beans specification.<br>
+ * limited by restrictions of the java-beans specification. Further it fully
+ * supports the generic type-system introduced with Java5.<br>
  * Further there are many advanced features such as mapped or indexed 
  * getter/setter support, support to add and remove items from a container 
  * property, etc.
