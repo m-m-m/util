32704a58955b768fba240f9a5d047f6db1454184
Joerg Hohwiller <joerg@j-hohwiller.de>
2009-06-05 15:11:23 +0000

    moved here

---
+++ mmm-util/mmm-util-pojo/src/main/java/net/sf/mmm/util/value/impl/ValueConverterToCompatiblePojo.java
@@ -0,0 +1,129 @@
+/* $Id$
+ * Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+package net.sf.mmm.util.value.impl;
+
+import javax.annotation.Resource;
+
+import net.sf.mmm.util.pojo.api.PojoFactory;
+import net.sf.mmm.util.pojo.descriptor.api.PojoDescriptorBuilder;
+import net.sf.mmm.util.pojo.descriptor.api.PojoDescriptorBuilderFactory;
+import net.sf.mmm.util.reflect.api.GenericType;
+import net.sf.mmm.util.value.api.ValueException;
+import net.sf.mmm.util.value.base.AbstractRecursiveValueConverter;
+
+/**
+ * This is an implementation of the
+ * {@link net.sf.mmm.util.value.api.ValueConverter} interface that converts an
+ * POJO to a POJO with the same properties. E.g. this can be useful when you
+ * have generated transport-objects (maybe from some strange
+ * web-service-framework) and want to convert those from or to your nice
+ * equivalent handwritten POJOs.
+ * 
+ * @author Joerg Hohwiller (hohwille at users.sourceforge.net)
+ */
+public class ValueConverterToCompatiblePojo extends AbstractRecursiveValueConverter<Object, Object> {
+
+  /** @see #setPojoFactory(PojoFactory) */
+  private PojoFactory pojoFactory;
+
+  private PojoDescriptorBuilder pojoDescriptorBuilder;
+
+  private PojoDescriptorBuilderFactory pojoDescriptorBuilderFactory;
+
+  /**
+   * The constructor.
+   */
+  public ValueConverterToCompatiblePojo() {
+
+    super();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public Class<Object> getSourceType() {
+
+    return Object.class;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public Class<Object> getTargetType() {
+
+    return Object.class;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @SuppressWarnings("unchecked")
+  public Object convert(Object value, Object valueSource, GenericType<? extends Object> targetType)
+      throws ValueException {
+
+    if (value == null) {
+      return null;
+    }
+    Class<?> valueClass = value.getClass();
+    Class<?> targetClass = targetType.getAssignmentClass();
+    if (valueClass.isEnum()) {
+      if (targetClass.isEnum()) {
+        Enum<?> enumValue = (Enum<?>) value;
+        return Enum.valueOf((Class<? extends Enum>) targetClass, enumValue.name());
+      }
+    }
+    return null;
+  }
+
+  /**
+   * This method sets the {@link PojoFactory} instance to used to create new
+   * instances of POJOs.
+   * 
+   * @param pojoFactory is the {@link PojoFactory} to use.
+   */
+  @Resource
+  public void setPojoFactory(PojoFactory pojoFactory) {
+
+    getInitializationState().requireNotInitilized();
+    this.pojoFactory = pojoFactory;
+  }
+
+  /**
+   * This method sets the {@link PojoDescriptorBuilder} instance to use. If no
+   * such instance is set, the
+   * {@link #setPojoDescriptorBuilderFactory(PojoDescriptorBuilderFactory)
+   * factory} has to be set and an individual instance will be created
+   * automatically on {@link #initialize() initialization}.
+   * 
+   * @param pojoDescriptorBuilder is the {@link PojoDescriptorBuilder} to use.
+   */
+  public void setPojoDescriptorBuilder(PojoDescriptorBuilder pojoDescriptorBuilder) {
+
+    getInitializationState().requireNotInitilized();
+    this.pojoDescriptorBuilder = pojoDescriptorBuilder;
+  }
+
+  /**
+   * This method sets the {@link PojoDescriptorBuilderFactory} instance to use.
+   * 
+   * @param pojoDescriptorBuilderFactory is the pojoDescriptorBuilderFactory to
+   *        set
+   */
+  @Resource
+  public void setPojoDescriptorBuilderFactory(
+      PojoDescriptorBuilderFactory pojoDescriptorBuilderFactory) {
+
+    getInitializationState().requireNotInitilized();
+    this.pojoDescriptorBuilderFactory = pojoDescriptorBuilderFactory;
+  }
+
+  /**
+   * @return the pojoDescriptorBuilderFactory
+   */
+  protected PojoDescriptorBuilderFactory getPojoDescriptorBuilderFactory() {
+
+    return this.pojoDescriptorBuilderFactory;
+  }
+
+}
