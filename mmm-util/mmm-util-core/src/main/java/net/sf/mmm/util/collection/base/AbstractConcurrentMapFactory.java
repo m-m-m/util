1137d7a19acc8fadf60a05a4bd4b62cbc963514e
Joerg Hohwiller <joerg@j-hohwiller.de>
2009-10-09 20:32:24 +0000

    Added Factory for ConcurrentMap.

---
+++ mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/collection/base/AbstractConcurrentMapFactory.java
@@ -0,0 +1,52 @@
+/* $Id$
+ * Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+package net.sf.mmm.util.collection.base;
+
+import java.util.concurrent.ConcurrentMap;
+
+import net.sf.mmm.util.collection.api.ConcurrentMapFactory;
+
+/**
+ * This is the abstract base implementation of the {@link ConcurrentMapFactory}
+ * interface.
+ * 
+ * @author Joerg Hohwiller (hohwille at users.sourceforge.net)
+ * @since 1.0.1
+ */
+@SuppressWarnings("unchecked")
+public abstract class AbstractConcurrentMapFactory implements ConcurrentMapFactory {
+
+  /**
+   * The constructor.
+   */
+  public AbstractConcurrentMapFactory() {
+
+    super();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public Class<ConcurrentMap> getMapInterface() {
+
+    return ConcurrentMap.class;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public ConcurrentMap createGeneric() {
+
+    return create();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public ConcurrentMap createGeneric(int capacity) {
+
+    return create(capacity);
+  }
+
+}
