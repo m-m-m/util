0626a36527d2ccb8482148b59772e2646204fccb
Joerg Hohwiller <joerg@j-hohwiller.de>
2010-01-01 20:23:18 +0000

    keySet() support

--- mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/pojo/descriptor/base/PojoMap.java
+++ mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/pojo/descriptor/base/PojoMap.java
@@ -3,9 +3,15 @@
  * http://www.apache.org/licenses/LICENSE-2.0 */
 package net.sf.mmm.util.pojo.descriptor.base;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import net.sf.mmm.util.collection.base.AbstractSimpleMap;
 import net.sf.mmm.util.pojo.descriptor.api.PojoDescriptor;
 import net.sf.mmm.util.pojo.descriptor.api.PojoDescriptorBuilder;
+import net.sf.mmm.util.pojo.descriptor.api.PojoPropertyDescriptor;
+import net.sf.mmm.util.pojo.descriptor.api.accessor.PojoPropertyAccessorNonArgMode;
 
 /**
  * This is represents a given {@link net.sf.mmm.util.pojo.api.Pojo} as
@@ -25,6 +31,9 @@ public class PojoMap extends AbstractSimpleMap<String, Object> {
   /** @see #get(Object) */
   private final Object pojo;
 
+  /** @see #keySet() */
+  private Set<String> keySet;
+
   /**
    * The constructor.
    * 
@@ -37,6 +46,7 @@ public class PojoMap extends AbstractSimpleMap<String, Object> {
     super();
     this.pojoDescriptor = pojoDescriptorBuilder.getDescriptor(pojo.getClass());
     this.pojo = pojo;
+    this.keySet = null;
   }
 
   /**
@@ -49,4 +59,22 @@ public class PojoMap extends AbstractSimpleMap<String, Object> {
     return this.pojoDescriptor.getProperty(this.pojo, pojoPath);
   }
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Set<String> keySet() {
+
+    if (this.keySet == null) {
+      Set<String> keys = new HashSet<String>();
+      PojoDescriptor<?> descriptor = this.pojoDescriptor;
+      for (PojoPropertyDescriptor propertyDescriptor : descriptor.getPropertyDescriptors()) {
+        if (propertyDescriptor.getAccessor(PojoPropertyAccessorNonArgMode.GET) != null) {
+          keys.add(propertyDescriptor.getName());
+        }
+      }
+      this.keySet = Collections.unmodifiableSet(keys);
+    }
+    return this.keySet;
+  }
 }
