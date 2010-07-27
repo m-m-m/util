6be54b1277f45e8668bc65cd16d9a229307357ce
Joerg Hohwiller <joerg@j-hohwiller.de>
2010-07-27 19:52:25 +0000

    improved: Now componentId is not required to be equal to interface name, but warning is logged then

--- mmm-test/src/main/java/net/sf/mmm/framework/base/SpringContainer.java
+++ mmm-test/src/main/java/net/sf/mmm/framework/base/SpringContainer.java
@@ -6,6 +6,8 @@ package net.sf.mmm.framework.base;
 import net.sf.mmm.framework.api.IocContainer;
 import net.sf.mmm.framework.api.IocContainerException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -19,6 +21,9 @@ public class SpringContainer implements IocContainer {
 
   /** The {@link ConfigurableApplicationContext} to delegate to. */
   private final ConfigurableApplicationContext applicationContext;
+
+  /** The {@link Logger}. */
+  private final Logger logger;
 
   /**
    * The constructor.
@@ -41,14 +46,33 @@ public class SpringContainer implements IocContainer {
 
     super();
     this.applicationContext = applicationContext;
+    this.logger = LoggerFactory.getLogger(getClass());
   }
 
   /**
    * {@inheritDoc}
    */
+  @SuppressWarnings("unchecked")
   public <COMPONENT_API> COMPONENT_API getComponent(Class<COMPONENT_API> apiClass) {
 
-    return getComponent(apiClass, apiClass.getName());
+    // return getComponent(apiClass, apiClass.getName());
+    COMPONENT_API component = null;
+    String[] componentIds = this.applicationContext.getBeanNamesForType(apiClass);
+    if ((componentIds != null) && (componentIds.length > 0)) {
+      if (componentIds.length > 1) {
+        throw new IocContainerException("Component '" + apiClass.getSimpleName()
+            + "' is ambiguous!\n" + componentIds);
+      }
+      String componentId = componentIds[0];
+      if (!componentId.equals(apiClass.getName())) {
+        this.logger.warn("Component-ID for '" + apiClass.getName() + "' is '" + componentId + "'!");
+      }
+      component = (COMPONENT_API) this.applicationContext.getBean(componentId);
+    }
+    if (component == null) {
+      throw new IocContainerException("Component '" + apiClass.getSimpleName() + "' NOT found!");
+    }
+    return component;
   }
 
   /**
