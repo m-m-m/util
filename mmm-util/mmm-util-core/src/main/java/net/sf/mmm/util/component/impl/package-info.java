d21576a896165427959a3bdf99f82402499c495e
Joerg Hohwiller <joerg@j-hohwiller.de>
2010-10-04 19:33:24 +0000

    added for javadoc

---
+++ mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/component/impl/package-info.java
@@ -0,0 +1,11 @@
+/* $Id$
+ * Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+/**
+ * Contains implementation of component-util.
+ * <a name="documentation"><h2>Component-Util Impl</h2></a>
+ * Provides {@link net.sf.mmm.util.component.impl.SpringContainer}
+ * that binds to the springframework.
+ */
+package net.sf.mmm.util.component.impl;
+
