ce92012e7bba598bc9981363c116ecf8a0639931
Joerg Hohwiller <joerg@j-hohwiller.de>
2010-11-24 23:13:20 +0000

    NLS: extracted logic to abstract parent classes

---
+++ mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/nls/base/BasicNlsMessage.java
@@ -0,0 +1,137 @@
+/* $Id$
+ * Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+package net.sf.mmm.util.nls.base;
+
+import java.util.Collections;
+import java.util.Map;
+
+import net.sf.mmm.util.nls.api.NlsAccess;
+import net.sf.mmm.util.nls.api.NlsTemplate;
+import net.sf.mmm.util.nls.api.NlsTemplateResolver;
+
+/**
+ * This is the abstract implementation of
+ * {@link net.sf.mmm.util.nls.api.NlsMessage} with the basic features.<br>
+ * You should extend this class whenever suitable to implement
+ * {@link net.sf.mmm.util.nls.api.NlsMessage}.
+ * 
+ * @author Joerg Hohwiller (hohwille at users.sourceforge.net)
+ * @since 2.0.1
+ */
+public abstract class BasicNlsMessage extends AbstractNlsMessage {
+
+  /** The {@link #message} as {@link NlsTemplate}. */
+  private NlsTemplate template;
+
+  /** @see #getInternationalizedMessage() */
+  private String message;
+
+  /** @see #getArgument(String) */
+  private final Map<String, Object> arguments;
+
+  /**
+   * The constructor.
+   * 
+   * @param template is the {@link NlsTemplate} for the
+   *        {@link #getInternationalizedMessage() raw message}.
+   * @param messageArguments are the {@link #getArgument(String) arguments}
+   *        filled into the message after nationalization.
+   */
+  public BasicNlsMessage(NlsTemplate template, Map<String, Object> messageArguments) {
+
+    super();
+    assert (template != null);
+    this.template = template;
+    this.message = null;
+    if (messageArguments == null) {
+      this.arguments = Collections.emptyMap();
+    } else {
+      this.arguments = messageArguments;
+    }
+  }
+
+  /**
+   * The constructor.
+   * 
+   * @param internationalizedMessage is the
+   *        {@link #getInternationalizedMessage() internationalized message}.
+   * @param messageArguments are the {@link #getArgument(String) arguments}
+   *        filled into the message after nationalization.
+   */
+  public BasicNlsMessage(String internationalizedMessage, Map<String, Object> messageArguments) {
+
+    super();
+    assert (internationalizedMessage != null);
+    this.template = null;
+    this.message = internationalizedMessage;
+    this.arguments = messageArguments;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public Object getArgument(String key) {
+
+    return this.arguments.get(key);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public String getInternationalizedMessage() {
+
+    if (this.message == null) {
+      this.message = this.template.translate(LOCALE_ROOT);
+    }
+    return this.message;
+  }
+
+  /**
+   * This method gets the message {@link #getArgument(String) arguments}.
+   * 
+   * @return the {@link Map} with the arguments.
+   */
+  protected Map<String, Object> getArguments() {
+
+    return this.arguments;
+  }
+
+  /**
+   * This method gets the {@link NlsTemplate} of this message.
+   * 
+   * @return the text the {@link NlsTemplate} or <code>null</code> if NOT yet
+   *         {@link NlsTemplateResolver#resolveTemplate(String) resolved}.
+   */
+  public NlsTemplate getTemplate() {
+
+    return this.template;
+  }
+
+  /**
+   * This method gets the {@link NlsTemplate} of this message.
+   * 
+   * @param resolver is the {@link NlsTemplateResolver} used to
+   *        {@link NlsTemplateResolver#resolveTemplate(String) resolve} the
+   *        {@link NlsTemplate} if NOT yet available.
+   * @return the text the {@link NlsTemplate}.
+   */
+  public NlsTemplate getTemplate(NlsTemplateResolver resolver) {
+
+    if (this.template == null) {
+      synchronized (this) {
+        if (this.template == null) {
+          NlsTemplateResolver templateResolver;
+          if (resolver == null) {
+            templateResolver = NlsAccess.getTemplateResolver();
+          } else {
+            templateResolver = resolver;
+          }
+          this.template = templateResolver.resolveTemplate(this.message);
+        }
+      }
+    }
+    return this.template;
+  }
+
+}
--- mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/nls/impl/NlsMessageImpl.java
+++ mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/nls/impl/NlsMessageImpl.java
@@ -4,43 +4,28 @@
 package net.sf.mmm.util.nls.impl;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Locale;
 import java.util.Map;
 
 import net.sf.mmm.util.io.api.IoMode;
 import net.sf.mmm.util.io.api.RuntimeIoException;
-import net.sf.mmm.util.nls.api.NlsAccess;
 import net.sf.mmm.util.nls.api.NlsMessage;
 import net.sf.mmm.util.nls.api.NlsTemplate;
 import net.sf.mmm.util.nls.api.NlsTemplateResolver;
+import net.sf.mmm.util.nls.base.BasicNlsMessage;
 import net.sf.mmm.util.nls.base.NlsDependencies;
 import net.sf.mmm.util.nls.impl.formatter.NlsMessageFormatterImpl;
 
 /**
- * This is the abstract base implementation of {@link NlsMessage}.<br>
- * You should extend this class rather than directly implementing the
- * {@link NlsMessage} interface to gain compatibility with further releases.
+ * This is the implementation of {@link net.sf.mmm.util.nls.api.NlsMessage}.
  * 
  * @author Joerg Hohwiller (hohwille at users.sourceforge.net)
  * @since 1.0.0
  */
-public class NlsMessageImpl implements NlsMessage {
-
-  /** Locale.ROOT is only available since java 6. */
-  private static final Locale LOCALE_ROOT = new Locale("");
+public class NlsMessageImpl extends BasicNlsMessage {
 
   /** @see #getNlsDependencies() */
   private final NlsDependencies nlsDependencies;
-
-  /** The {@link #message} as {@link NlsTemplate}. */
-  private NlsTemplate template;
-
-  /** @see #getInternationalizedMessage() */
-  private String message;
-
-  /** @see #getArgument(String) */
-  private final Map<String, Object> arguments;
 
   /**
    * The constructor.
@@ -54,16 +39,8 @@ public class NlsMessageImpl implements NlsMessage {
   public NlsMessageImpl(NlsTemplate template, Map<String, Object> messageArguments,
       NlsDependencies nlsDependencies) {
 
-    super();
-    assert (template != null);
+    super(template, messageArguments);
     this.nlsDependencies = nlsDependencies;
-    this.template = template;
-    this.message = null;
-    if (messageArguments == null) {
-      this.arguments = Collections.emptyMap();
-    } else {
-      this.arguments = messageArguments;
-    }
   }
 
   /**
@@ -78,54 +55,8 @@ public class NlsMessageImpl implements NlsMessage {
   public NlsMessageImpl(String internationalizedMessage, Map<String, Object> messageArguments,
       NlsDependencies nlsDependencies) {
 
-    super();
-    assert (internationalizedMessage != null);
+    super(internationalizedMessage, messageArguments);
     this.nlsDependencies = nlsDependencies;
-    this.template = null;
-    this.message = internationalizedMessage;
-    this.arguments = messageArguments;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public Object getArgument(String key) {
-
-    return this.arguments.get(key);
-  }
-
-  /**
-   * This method gets the {@link #getArgument(String) Argument} for the given
-   * index.
-   * 
-   * @param index is the index of the requested argument.
-   * @return the argument at the given index or <code>null</code> if no such
-   *         argument exists.
-   * @deprecated use {@link #getArgument(String)}
-   */
-  @Deprecated
-  public Object getArgument(int index) {
-
-    return getArgument(Integer.toString(index));
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public int getArgumentCount() {
-
-    return 0;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public String getInternationalizedMessage() {
-
-    if (this.message == null) {
-      this.message = this.template.translate(LOCALE_ROOT);
-    }
-    return this.message;
   }
 
   /**
@@ -137,81 +68,20 @@ public class NlsMessageImpl implements NlsMessage {
   }
 
   /**
-   * This method gets the {@link NlsTemplate} of this message.
-   * 
-   * @return the text the {@link NlsTemplate} or <code>null</code> if NOT yet
-   *         {@link NlsTemplateResolver#resolveTemplate(String) resolved}.
-   */
-  public NlsTemplate getTemplate() {
-
-    return this.template;
-  }
-
-  /**
-   * This method gets the {@link NlsTemplate} of this message.
-   * 
-   * @param resolver is the {@link NlsTemplateResolver} used to
-   *        {@link NlsTemplateResolver#resolveTemplate(String) resolve} the
-   *        {@link NlsTemplate} if NOT yet available.
-   * @return the text the {@link NlsTemplate}.
-   */
-  public NlsTemplate getTemplate(NlsTemplateResolver resolver) {
-
-    if (this.template == null) {
-      synchronized (this) {
-        if (this.template == null) {
-          NlsTemplateResolver templateResolver;
-          if (resolver == null) {
-            templateResolver = NlsAccess.getTemplateResolver();
-          } else {
-            templateResolver = resolver;
-          }
-          this.template = templateResolver.resolveTemplate(this.message);
-        }
-      }
-    }
-    return this.template;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public String getLocalizedMessage() {
-
-    return getLocalizedMessage(Locale.getDefault());
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public String getLocalizedMessage(Locale locale) {
-
-    return getLocalizedMessage(locale, null);
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public String getLocalizedMessage(Locale locale, NlsTemplateResolver resolver) {
-
-    StringBuilder result = new StringBuilder(this.message.length() + 16);
-    getLocalizedMessage(locale, resolver, result);
-    return result.toString();
-  }
-
-  /**
    * {@inheritDoc}
    */
   public void getLocalizedMessage(Locale locale, NlsTemplateResolver resolver, Appendable buffer) {
 
     try {
-      NlsTemplate nlsTemplate = this.template;
+      NlsTemplate nlsTemplate = getTemplate();
       if (nlsTemplate == null) {
         if (locale != LOCALE_ROOT) {
           nlsTemplate = getTemplate(resolver);
         }
       }
-      if (this.arguments.isEmpty()) {
+      String message = getInternationalizedMessage();
+      Map<String, Object> arguments = getArguments();
+      if (arguments.isEmpty()) {
         String text = null;
         if (nlsTemplate != null) {
           text = nlsTemplate.translate(locale);
@@ -220,14 +90,14 @@ public class NlsMessageImpl implements NlsMessage {
           if (locale != LOCALE_ROOT) {
             buffer.append(LOCALIZATION_FAILURE_PREFIX);
           }
-          text = this.message;
+          text = message;
         }
         buffer.append(text);
       } else {
         boolean success = false;
         if (nlsTemplate != null) {
-          success = nlsTemplate.translate(locale, this.arguments, buffer, resolver,
-              this.nlsDependencies);
+          success = nlsTemplate
+              .translate(locale, arguments, buffer, resolver, this.nlsDependencies);
         }
         if (!success) {
           // This does not make sense:
@@ -237,9 +107,9 @@ public class NlsMessageImpl implements NlsMessage {
           // if (locale != LOCALE_ROOT) {
           // buffer.append(LOCALIZATION_FAILURE_PREFIX);
           // }
-          NlsMessageFormatterImpl format = new NlsMessageFormatterImpl(this.message,
+          NlsMessageFormatterImpl format = new NlsMessageFormatterImpl(message,
               getNlsDependencies());
-          format.format(null, locale, this.arguments, resolver, buffer);
+          format.format(null, locale, arguments, resolver, buffer);
         }
       }
     } catch (IOException e) {
@@ -250,6 +120,7 @@ public class NlsMessageImpl implements NlsMessage {
   /**
    * {@inheritDoc}
    */
+  @Override
   public String getMessage() {
 
     return getLocalizedMessage(LOCALE_ROOT, null);
@@ -258,6 +129,7 @@ public class NlsMessageImpl implements NlsMessage {
   /**
    * {@inheritDoc}
    */
+  @Override
   public NlsMessage toNlsMessage() {
 
     return this;
