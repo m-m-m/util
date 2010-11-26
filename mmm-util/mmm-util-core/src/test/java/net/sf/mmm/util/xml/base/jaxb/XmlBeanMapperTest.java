268802076cd85c3635ccf30f478cead1efb1038e
Joerg Hohwiller <joerg@j-hohwiller.de>
2010-11-26 20:46:22 +0000

    improved test coverage of JAXB stuff

--- mmm-util/mmm-util-core/src/test/java/net/sf/mmm/util/xml/base/jaxb/XmlBeanMapperTest.java
+++ mmm-util/mmm-util-core/src/test/java/net/sf/mmm/util/xml/base/jaxb/XmlBeanMapperTest.java
@@ -5,6 +5,7 @@ package net.sf.mmm.util.xml.base.jaxb;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.util.Properties;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -13,6 +14,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlID;
 import javax.xml.bind.annotation.XmlIDREF;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import junit.framework.Assert;
 import net.sf.mmm.test.ExceptionHelper;
@@ -41,9 +43,14 @@ public class XmlBeanMapperTest {
     bean.nodes = new XmlNode[2];
     bean.nodes[0] = new XmlNode();
     bean.nodes[0].id = "root";
+    bean.nodes[0].test = TestEnum.FOO;
     bean.nodes[1] = new XmlNode();
     bean.nodes[1].id = "child";
     bean.nodes[1].parent = bean.nodes[0];
+    bean.nodes[1].test = TestEnum.SOMETHING_ELSE;
+    bean.properties = new Properties();
+    bean.properties.setProperty("foo", "42");
+    bean.properties.setProperty("bar", "magic");
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     mapper.saveXml(bean, outputStream);
     byte[] xmlBytes = outputStream.toByteArray();
@@ -57,9 +64,41 @@ public class XmlBeanMapperTest {
     Assert.assertNotNull(loadedBean.nodes[0]);
     Assert.assertEquals("root", loadedBean.nodes[0].id);
     Assert.assertNull(loadedBean.nodes[0].parent);
+    Assert.assertSame(TestEnum.FOO, loadedBean.nodes[0].test);
     Assert.assertNotNull(loadedBean.nodes[1]);
     Assert.assertEquals("child", loadedBean.nodes[1].id);
     Assert.assertSame(loadedBean.nodes[0], loadedBean.nodes[1].parent);
+    Assert.assertSame(TestEnum.SOMETHING_ELSE, loadedBean.nodes[1].test);
+    Assert.assertNotNull(loadedBean.properties);
+    Assert.assertEquals(bean.properties, loadedBean.properties);
+  }
+
+  /**
+   * Tests {@link XmlBeanMapper#loadXml(java.io.InputStream, Object)}.
+   */
+  public void testLoadXml() {
+
+    String xml = "<?xml version='1.0'?><bean><node id='root' test='something-else'/>"
+        + "<node id='child' parent='root' test='SOMETHING_ELSE'/>"
+        + "<properties><property key='foo'>42</property></properties></bean>";
+    XmlBeanMapper<XmlBean> mapper = new XmlBeanMapper<XmlBeanMapperTest.XmlBean>(XmlBean.class);
+    mapper.initialize();
+    ByteArrayInputStream inputStream = new ByteArrayInputStream(xml.getBytes());
+    XmlBean loadedBean = mapper.loadXml(inputStream, "test-case");
+    Assert.assertNotNull(loadedBean);
+    Assert.assertNotNull(loadedBean.nodes);
+    Assert.assertEquals(2, loadedBean.nodes.length);
+    Assert.assertNotNull(loadedBean.nodes[0]);
+    Assert.assertEquals("root", loadedBean.nodes[0].id);
+    Assert.assertNull(loadedBean.nodes[0].parent);
+    Assert.assertSame(TestEnum.SOMETHING_ELSE, loadedBean.nodes[0].test);
+    Assert.assertNotNull(loadedBean.nodes[1]);
+    Assert.assertEquals("child", loadedBean.nodes[1].id);
+    Assert.assertSame(loadedBean.nodes[0], loadedBean.nodes[1].parent);
+    Assert.assertSame(TestEnum.SOMETHING_ELSE, loadedBean.nodes[1].test);
+    Assert.assertNotNull(loadedBean.properties);
+    Assert.assertEquals(1, loadedBean.properties.size());
+    Assert.assertEquals("42", loadedBean.properties.getProperty("foo"));
   }
 
   @Test
@@ -88,6 +127,11 @@ public class XmlBeanMapperTest {
 
     @XmlElement(name = "node")
     private XmlNode[] nodes;
+
+    @XmlJavaTypeAdapter(value = XmlAdapterProperties.class)
+    @XmlElement(name = "properties")
+    private Properties properties;
+
   }
 
   public static class XmlNode {
@@ -99,6 +143,30 @@ public class XmlBeanMapperTest {
     @XmlIDREF
     @XmlAttribute(name = "parent")
     private XmlNode parent;
+
+    @XmlJavaTypeAdapter(value = XmlAdapterTestEnum.class)
+    @XmlAttribute(name = "test")
+    private TestEnum test;
+
+  }
+
+  protected static enum TestEnum {
+
+    FOO,
+
+    SOMETHING_ELSE
+  }
+
+  protected static class XmlAdapterTestEnum extends XmlAdapterEnum<TestEnum> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Class<TestEnum> getEnumClass() {
+
+      return TestEnum.class;
+    }
 
   }
 
