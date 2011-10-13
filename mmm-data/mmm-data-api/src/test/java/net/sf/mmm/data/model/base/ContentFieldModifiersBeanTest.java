c8a52f7f6eeccf813b90598e9a43487b4a4bc15e
Joerg Hohwiller <joerg@j-hohwiller.de>
2011-10-13 22:43:58 +0000

    refactoring content->name part 5

--- mmm-data/mmm-data-api/src/test/java/net/sf/mmm/data/model/base/ContentFieldModifiersBeanTest.java
+++ mmm-data/mmm-data-api/src/test/java/net/sf/mmm/data/model/base/ContentFieldModifiersBeanTest.java
@@ -1,7 +1,7 @@
 /* $Id$
  * Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0 */
-package net.sf.mmm.content.model.base;
+package net.sf.mmm.data.model.base;
 
 import junit.framework.TestCase;
 import net.sf.mmm.data.reflection.api.ContentFieldModifiers;
