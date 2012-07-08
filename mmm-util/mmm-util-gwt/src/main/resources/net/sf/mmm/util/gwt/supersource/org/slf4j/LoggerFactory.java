d0bf6a6e642b43fa6bed36a8f66571aa07ad0ab4
Joerg Hohwiller <joerg@j-hohwiller.de>
2012-07-08 22:45:33 +0200

    * #15: GWT support for util-core fixed (once more)

--- mmm-util/mmm-util-gwt/src/main/resources/net/sf/mmm/util/gwt/supersource/org/slf4j/LoggerFactory.java
+++ mmm-util/mmm-util-gwt/src/main/resources/net/sf/mmm/util/gwt/supersource/org/slf4j/LoggerFactory.java
@@ -1,12 +1,13 @@
 /* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0 */
+// CHECKSTYLE:OFF (GWT compatible copy)
 package org.slf4j;
 
 import net.sf.mmm.util.nls.impl.GwtLogger;
 
 /**
  * This is the GWT compatible implementation of {@link org.slf4j.LoggerFactory}.
- * 
+ *
  * @author Joerg Hohwiller (hohwille at users.sourceforge.net)
  * @since 1.0.0
  */
@@ -14,7 +15,7 @@ public final class LoggerFactory {
 
   /**
    * This method creates a {@link Logger} for the given {@link Class}.
-   * 
+   *
    * @param type is the {@link Class} the logger will be named after.
    * @return the new {@link Logger}.
    */
@@ -26,7 +27,7 @@ public final class LoggerFactory {
 
   /**
    * This method creates a {@link Logger} for the given <code>name</code>.
-   * 
+   *
    * @param name is the {@link Logger#getName() name} of the new logger.
    * @return the new {@link Logger}.
    */
@@ -36,3 +37,4 @@ public final class LoggerFactory {
   }
 
 }
+// CHECKSTYLE:OFF
---
+++ mmm-util/mmm-util-gwt/src/main/resources/net/sf/mmm/util/gwt/supersource/org/slf4j/helpers/NamedLoggerBase.java
@@ -0,0 +1,39 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+// CHECKSTYLE:OFF (GWT compatible copy)
+package org.slf4j.helpers;
+
+import java.io.Serializable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * GWT compatible version of this class.
+ *
+ * @author Ceki Gulcu
+ * @since 1.5.3
+ */
+abstract class NamedLoggerBase implements Logger, Serializable {
+
+  /** UID for serialization. */
+  private static final long serialVersionUID = 7535258609338176893L;
+
+  /** @see #getName() */
+  protected String name;
+
+  /**
+   * {@inheritDoc}
+   */
+  public String getName() {
+    return this.name;
+  }
+
+  protected Object readResolve() {
+    // using getName() instead of this.name works even for
+    // NOPLogger
+    return LoggerFactory.getLogger(getName());
+  }
+
+}
+// CHECKSTYLE:ON
