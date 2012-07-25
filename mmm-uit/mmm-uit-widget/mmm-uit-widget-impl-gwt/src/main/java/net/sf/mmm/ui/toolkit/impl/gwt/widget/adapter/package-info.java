62bdd68dbb70353d5e643b377bc0974305010345
Joerg Hohwiller <joerg@j-hohwiller.de>
2012-07-26 01:54:31 +0200

    added missing stuff

---
+++ mmm-uit/mmm-uit-widget/mmm-uit-widget-impl-gwt/src/main/java/net/sf/mmm/ui/toolkit/impl/gwt/widget/adapter/package-info.java
@@ -0,0 +1,10 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+/**
+ * Contains the implementation for widget adapters using GWT.
+ * <a name="documentation"/><h2>UIT Impl GWT Widget Adapter</h2>
+ * This package contains the implementation of {@link net.sf.mmm.ui.toolkit.base.widget.adapter.UiWidgetAdapter} using GWT
+ * (Google Web Toolkit).
+ */
+package net.sf.mmm.ui.toolkit.impl.gwt.widget.adapter;
+
---
+++ mmm-util/mmm-util-gwt/src/main/java/net/sf/mmm/util/gwt/api/package-info.java
@@ -0,0 +1,9 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+/**
+ * Provides the API for GWT specific utilities.
+ * <a name="documentation"/><h2>GWT-Util API</h2>
+ * This is the package of utilities for GWT (Google Web Toolkit) such as .
+ */
+package net.sf.mmm.util.gwt.api;
+
