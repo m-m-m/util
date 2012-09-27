07dc73b066099c79e174ebb002133cf8585a4f3c
Joerg Hohwiller <joerg@j-hohwiller.de>
2012-09-27 22:00:19 +0200

    various improvements

---
+++ mmm-data/mmm-data-impl-persistence/src/main/java/net/sf/mmm/data/impl/entity/pim/contact/DataContactManagerImpl.java
@@ -0,0 +1,55 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+package net.sf.mmm.data.impl.entity.pim.contact;
+
+import javax.inject.Named;
+
+import net.sf.mmm.data.api.entity.pim.contact.DataContact;
+import net.sf.mmm.data.api.entity.pim.contact.DataContactView;
+import net.sf.mmm.persistence.impl.jpa.JpaPersistenceEntityManager;
+
+/**
+ * TODO: this class ...
+ * 
+ * @author Joerg Hohwiller (hohwille at users.sourceforge.net)
+ * @since 1.0.0
+ */
+@Named
+public class DataContactManagerImpl extends JpaPersistenceEntityManager<Long, DataContact> {
+
+  /**
+   * The constructor.
+   */
+  public DataContactManagerImpl() {
+
+    super();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Class<? extends DataContact> getEntityClassImplementation() {
+
+    return DataContactImpl.class;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Class<? super DataContact> getEntityClassReadOnly() {
+
+    return DataContactView.class;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Class<DataContact> getEntityClassReadWrite() {
+
+    return DataContact.class;
+  }
+
+}
