23a7fb9b00654b603d2c2601dae34892af827b3e
Joerg Hohwiller <joerg@j-hohwiller.de>
2012-09-27 21:43:33 +0200

    28: improve CDI/spring configurations improved tx

---
+++ mmm-transaction/mmm-transaction-impl-jpa/src/main/java/net/sf/mmm/transaction/impl/jpa/package-info.java
@@ -0,0 +1,11 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+/**
+ * Provides the implementation for transaction support using JPA.
+ * <a name="documentation"/><h2>Transaction Impl JPA</h2>
+ * This package contains the implementation of the
+ * {@link net.sf.mmm.transaction.api Transaction API} using Java Persistence
+ * API (JPA).
+ */
+package net.sf.mmm.transaction.impl.jpa;
+
