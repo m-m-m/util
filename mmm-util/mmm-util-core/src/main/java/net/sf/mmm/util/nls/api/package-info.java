dcb7994cfeb38d983486f474d3e66f64f5df296c
Joerg Hohwiller <joerg@j-hohwiller.de>
2012-11-01 21:40:14 +0100

    javadoc updated and completed

--- mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/nls/api/package-info.java
+++ mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/nls/api/package-info.java
@@ -24,110 +24,145 @@
  * You will typically store your messages in a
  * {@link java.util.ResourceBundle}. Now if you store the
  * technical key of the bundle in a message or exception the receiver needs the
- * proper <code>{@link java.util.ResourceBundle}</code> to decode it or he ends 
+ * proper <code>{@link java.util.ResourceBundle}</code> to decode it or he ends
  * up with a cryptic message he can NOT understand (e.g. as illustrated by the
  * screenshot).<br>
- * On the other hand you need to know the locale of the receiver to do the l10n
- * when creating the message or exception with the proper text. This is
- * impossible to solve in may situations or leading to sick design.
+ * On the other hand you need to know the locale of the receiver to do the l10n when creating the message or exception
+ * with the proper text. This may lead to sick design such as static hacks.
  * </p>
  * <h3>The Solution</h3>
- * The solution is quite simple because again Java brings parts of the solution
- * with <code>{@link java.text.MessageFormat}</code>:<br>
+ * The solution is quite simple:<br>
  * We simply bundle the message in default language together with the separated
  * locale independent arguments in one container object that is called
- * <code>{@link net.sf.mmm.util.nls.api.NlsMessage}</code>. 
- * Here is an example to clarify the idea: The i18n
- * message is "Hi {name}! How are you?" and the language independent argument is
- * the users name e.g. "Lilli". Now if we store these informations together we
- * have all we need. To get the localized message we simply translate the i18n
- * message to the proper language and then fill in the arguments. If we can NOT
- * translate we always have the message in default language which is "Hi Lilli!
- * How are you?".<br>
- * But how do we translate the i18n message without artificial intelligence? The
- * answer is very easy: We simply use the bundle in default language and do a
- * reverse lookup to get the technical key. Then we can lookup the proper
- * message in the according bundle to translate it to the according locale.<br>
- * All you have to do is creating a subclass of
- * <code>{@link net.sf.mmm.util.nls.base.AbstractResourceBundle}</code> that declares 
- * public string constants.
- * 
+ * <code>{@link net.sf.mmm.util.nls.api.NlsMessage}</code>.
+ * For exceptions there is additional support via <code>{@link net.sf.mmm.util.nls.api.NlsException}</code> and
+ * <code>{@link net.sf.mmm.util.nls.api.NlsRuntimeException}</code>.
+ * Here is an example to clarify the idea of {@link net.sf.mmm.util.nls.api.NlsMessage}:
+ * The i18n message is "Hi {name}! How are you?" and the language independent argument is the users name e.g. "Lilli".
+ * Now if we store these informations together we have all we need. To get the localized message we simply translate the
+ * i18n message to the proper language and then fill in the arguments. If we can NOT translate we always have the
+ * message in default language which is "Hi Lilli! How are you?".<br>
+ * But how do we translate the i18n message without artificial intelligence?
+ * The answer is quite easy, but we offer two different approaches. These
+ * are described in the following sections:
+ * <h4>Solution 1: {@link net.sf.mmm.util.nls.api.NlsBundle}</h4>
+ * The recommended approach is to create an interface derived from {@link net.sf.mmm.util.nls.api.NlsBundle}.
+ * For each message you define a method that takes the arguments to fill in and returns an
+ * {@link net.sf.mmm.util.nls.api.NlsMessage}. Via annotations you provide the default message for each method.
  * <pre>
  * package foo.bar;
- * 
- * public class MyResourceBundle extends {@link net.sf.mmm.util.nls.base.AbstractResourceBundle} {
+ *
+ * @{@link net.sf.mmm.util.nls.api.NlsBundleLocation}(bundleName = "NlsBundleFooBar")
+ * public class NlsBundleFooBarRoot extends {@link net.sf.mmm.util.nls.api.NlsMessage} {
+ *
+ *   @{@link net.sf.mmm.util.nls.api.NlsBundleMessage}("Hi {name}! How are you?")
+ *   {@link net.sf.mmm.util.nls.api.NlsMessage} messageSayHi(@{@link javax.inject.Named}("name") String name);
+ *
+ *   @{@link net.sf.mmm.util.nls.api.NlsBundleMessage}("Sorry. The login \"{login}\" is already in use. Please choose a different login.")
+ *   {@link net.sf.mmm.util.nls.api.NlsMessage} errorLoginInUse(@{@link javax.inject.Named}("login") String login);
+ * }
+ * </pre>
+ * From your code you now can do this:
+ *
+ * <pre>
+ * String userName = "Lilli";
+ * {@link net.sf.mmm.util.nls.api.NlsMessage} msg = {@link net.sf.mmm.util.nls.api.NlsAccess#getBundleFactory()}.{@link
+ * net.sf.mmm.util.nls.api.NlsBundleFactory#createBundle(Class)
+ * createBundle}(NlsBundleFooBarRoot.class).messageSayHi(userName);
+ * String textDefault = msg.{@link net.sf.mmm.util.nls.api.NlsMessage#getLocalizedMessage() getLocalizedMessage}());
+ * String textDe = msg.{@link net.sf.mmm.util.nls.api.NlsMessage#getLocalizedMessage(java.util.Locale)
+ * getLocalizedMessage}({@link java.util.Locale}.GERMANY));
+ * </pre>
+ * For the error message create an exception like this:
+ * <pre>
+ * public class LoginAlreadyInUseException extends {@link net.sf.mmm.util.nls.api.NlsRuntimeException} {
+ *   public LoginAlreadyInUseException(String usedLogin) {
+ *
+ *     super({@link net.sf.mmm.util.nls.api.NlsRuntimeException#createBundle(Class)
+ *     createBundle}(NlsBundleFooBarRoot.class).errorLoginInUse(usedLogin));
+ *   }
+ * }
+ * </pre>
+ * For further details see {@link net.sf.mmm.util.nls.api.NlsBundle}.<br/>
+ * <br/>
+ * For localization you can create property files with the translations of your NLS-bundle.
+ * E.g. <code>foo/bar/NlsBundleFooBar_de.properties</code> with this content:
+ * <pre>
+ * messageSayHi = Hallo {name}! Wie geht es Dir?
+ * errorLoginInUse = Es tut uns leid. Das Login "{login}" ist bereits vergeben. Bitte wählen Sie ein anderes Login.
+ * </pre>
+ *
+ * In order to support you with creating and maintaining the localized
+ * properties, this solution also comes with the
+ * {@link net.sf.mmm.util.nls.base.ResourceBundleSynchronizer}.
+ * <br/>
+ * The advantage is that also the bundle name and key are available in the {@link net.sf.mmm.util.nls.api.NlsMessage}
+ * and that this approach is GWT compatible when using <code>mmm-util-gwt</code>. However, there is still our legacy
+ * approach.
+ * <h4>Solution 2: {@link net.sf.mmm.util.nls.base.AbstractResourceBundle}</h4>
+ * Simply create a subclass of {@link net.sf.mmm.util.nls.base.AbstractResourceBundle} that declares public string
+ * constants:
+ * <pre>
+ * package foo.bar;
+ *
+ * public class FooBarResourceBundle extends {@link net.sf.mmm.util.nls.base.AbstractResourceBundle} {
  *   public static final String MSG_SAY_HI = "Hi {name}! How are you?";
  *   public static final String ERR_LOGIN_IN_USE = "Sorry. The login \"{login}\" is " +
  *     "already in use. Please choose a different login.";
  * }
  * </pre>
- * 
- * For the automatic reverse-lookup create the file 
- * <code>META-INF/net.sf.mmm/nls-bundles</code> with the fully qualified name 
- * of your bundle-class (foo.bar.MyResourceBundle) as content.
- * 
  * From your code you only need to create the
  * {@link net.sf.mmm.util.nls.api.NlsMessage NlsMessage}</code> using this
  * constants:
- * 
- * <p>
- * <code>&nbsp;&nbsp;String userName = "Lilli";</code><br>
- * <code>&nbsp;&nbsp;{@link net.sf.mmm.util.nls.api.NlsMessage} msg = 
- * {@link net.sf.mmm.util.nls.api.NlsAccess#getFactory()}.<!--
- * -->{@link net.sf.mmm.util.nls.api.NlsMessageFactory#create(String, String, Object) create}<!--
- * -->(MyResourceBundle.MSG_SAY_HI, "name", userName);</code>
- * </p>
- * 
- * For exceptions there is additional support via 
- * <code>{@link net.sf.mmm.util.nls.api.NlsException}</code> and 
- * <code>{@link net.sf.mmm.util.nls.api.NlsRuntimeException}</code>. Simply
- * derive your declared exceptions from one of these classes:
- * 
+ *
+ * <pre>
+ * String userName = "Lilli";
+ * {@link net.sf.mmm.util.nls.api.NlsMessage} msg = {@link net.sf.mmm.util.nls.api.NlsAccess#getFactory()
+ * }.{@link net.sf.mmm.util.nls.api.NlsMessageFactory#create(String, String, Object)
+ * create}(FooBarResourceBundle.MSG_SAY_HI, "name", userName);</code>
+ * String textDefault = msg.{@link net.sf.mmm.util.nls.api.NlsMessage#getLocalizedMessage() getLocalizedMessage}());
+ * String textDe = msg.{@link net.sf.mmm.util.nls.api.NlsMessage#getLocalizedMessage(java.util.Locale)
+ * getLocalizedMessage}({@link java.util.Locale}.GERMANY));
+ * </pre>
+ * For the error message create an exception like this:
  * <pre>
  * public class LoginAlreadyInUseException extends {@link net.sf.mmm.util.nls.api.NlsException} {
  *   public LoginAlreadyInUseException(String usedLogin) {
- *     super(MyResourceBundle.ERR_LOGIN_IN_USE, usedLogin);
+ *     super(MyResourceBundle.ERR_LOGIN_IN_USE, toMap(KEY_NAME, usedLogin));
  *   }
  * }
  * </pre>
- * 
- * Now if you throw a <code>LoginAlreadyInUseException</code>, it will behave as a
- * "regular" exception with an English message. Additionally it has methods
- * <code>getLocalizedMessage</code> and <code>printStackTrace</code> that
- * take a {@link java.util.Locale} and optionally a 
- * <code>{@link net.sf.mmm.util.nls.api.NlsTemplateResolver}</code> 
- * as argument.
- * 
+ * For the automatic reverse-lookup create the file
+ * <code>META-INF/net.sf.mmm/nls-bundles</code> with the fully qualified name
+ * of your bundle-class (foo.bar.FooBarResourceBundle) as content.
+ * <br/>
  * For localization you can create property files with the translations of your
- * NLS-bundle. E.g. <code>foo/bar/MyResourceBundle_de.properties</code> with 
+ * NLS-bundle. E.g. <code>foo/bar/FooBarResourceBundle_de.properties</code> with
  * this content:
  * <pre>
  * MSG_SAY_HI = Hallo {name}! Wie geht es Dir?
- * LOGIN_IN_USE = Es tut uns leid. Das Login "{login}" ist bereits vergeben. Bitte wählen Sie ein anderes Login.
+ * ERR_LOGIN_IN_USE = Es tut uns leid. Das Login "{login}" ist bereits vergeben. Bitte wählen Sie ein anderes Login.
  * </pre>
- * 
- * In order to support you with creating and maintaining the localized 
- * properties, this solution also comes with the 
+ *
+ * In order to support you with creating and maintaining the localized
+ * properties, this solution also comes with the
  * {@link net.sf.mmm.util.nls.base.ResourceBundleSynchronizer}.
- * 
  * <h3>Conclusion</h3>
- * As we have seen the NLS provided here makes it very easy for developers to
- * write and maintain internationalized code. While messages are spread 
- * throughout the code there are only very view places where these messages are 
- * actually displayed to the end-user. At these places you need to figure out
- * the users locale.
- * <p>
- * The {@link net.sf.mmm.util.nls.api.NlsMessage} allows to store an 
- * internationalized message together with the language independent arguments.
- * </p>
- * <p>
- * The localization (translation to native language) is easily performed by 
- * {@link net.sf.mmm.util.nls.api.NlsMessage#getLocalizedMessage(java.util.Locale)}.
- * </p>
- * <p>
- * For exceptions there is additional support via 
- * {@link net.sf.mmm.util.nls.api.NlsThrowable}.
- * </p>
+ * As we have seen the NLS provided here makes it very easy for developers to write and maintain internationalized code.
+ * While messages are created throughout the code they only need to be localized for the end-user in the client and at
+ * service-endpoints. Only at these places you need to figure out the users locale.
+ * <ul>
+ *   <li>The {@link net.sf.mmm.util.nls.api.NlsMessage} allows to store an internationalized message together with
+ *   actual arguments to fill in.</li>
+ *   <li>The arguments can be arbitrary objects including {@link net.sf.mmm.util.nls.api.NlsMessage}s.</li>
+ *   <li>There are powerful ways to format these arguments including variable expressions for optional arguments or
+ *   plural forms. See {@link net.sf.mmm.util.nls.api.NlsMessage} for advanced examples.</li>
+ *   <li>In addition to numbered arguments that have been deprecated we also support named arguments. This makes
+ *   maintenance of the messages a lot easier. Your localizers will love you for choosing this solution.</li>
+ *   <li>The localization (translation to native language) is easily performed by
+ *   {@link net.sf.mmm.util.nls.api.NlsMessage#getLocalizedMessage(java.util.Locale)}.</li>
+ *   <li>For exceptions there is additional support via {@link net.sf.mmm.util.nls.api.NlsThrowable}.</li>
+ * </ul>
  */
 package net.sf.mmm.util.nls.api;
 
