7a04082408f6aa4a428dddf4cd3392292608f359
Joerg Hohwiller <joerg@j-hohwiller.de>
2013-02-13 23:44:44 +0100

    * removed obsolete scripts * solved checkstyle file reference problem

--- mmm-setup/src/scripts/linux/update-site.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd `dirname "${0}"`
-. include.inc
-cd ${MMM_BASE}
-pwd
-mvn site:stage -DstagingDirectory=${PWD}/target/stage/ | tee .maven-site.log
--- mmm-setup/src/scripts/windows/setup-all.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-REM $Id$
-
-CD %~dp0
-CALL setup-maven.bat
-CALL setup-subversion.bat
-CALL setup-eclipse.bat
