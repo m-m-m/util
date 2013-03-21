45ed2eea57262775c5d1cb56a547acfd9aec7fa1
Joerg Hohwiller <joerg@j-hohwiller.de>
2013-03-21 22:52:04 +0100

    added missing package-info

---
+++ mmm-client/mmm-client-ui/mmm-client-ui-widget/mmm-client-ui-widget-api/src/main/java/net/sf/mmm/client/ui/api/widget/factory/package-info.java
@@ -0,0 +1,13 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+/**
+ * Contains the API for widget factories.
+ * <a name="documentation"/><h2>Client UI API Widget Factory</h2>
+ * This package contains the internal API for widget factories such as {@link net.sf.mmm.client.ui.api.widget.factory.UiWidgetFactoryNative}
+ * and {@link net.sf.mmm.client.ui.api.widget.factory.UiWidgetFactoryDatatype}. Everything is aggregated into
+ * {@link net.sf.mmm.client.ui.api.widget.UiWidgetFactory} that should be used by end-users. The content of this package
+ * is for implementors of the API and should not directly be used by end-users.<br/>
+ * Please consult {@link net.sf.mmm.client.ui.api Client UI API} for an introduction.
+ */
+package net.sf.mmm.client.ui.api.widget.factory;
+
