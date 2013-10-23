d255cbef377a1ec37a2f962705f79f087c4dd279
Joerg Hohwiller <joerg@j-hohwiller.de>
2013-10-23 23:17:29 +0200

    removed own date alternative, we will use javax.time in java8

--- mmm-util/mmm-util-datatype/src/main/java/net/sf/mmm/util/date/base/package-info.java
+++ mmm-util/mmm-util-datatype/src/main/java/net/sf/mmm/util/date/base/package-info.java
@@ -3,7 +3,7 @@
 /**
  * Provides the basic implementation of utilities for dealing with date and time.
  * <a name="documentation"/><h2>Date-Util Base</h2>
- * This package contains the base implementation of 
+ * This package contains the base implementation of
  * {@link net.sf.mmm.util.date.api Date-Util API}.
  */
 package net.sf.mmm.util.date.base;
