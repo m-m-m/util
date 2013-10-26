233cdccd8b3b7ee251f63bf330350e2850da4b7e
Joerg Hohwiller <joerg@j-hohwiller.de>
2013-10-26 11:55:06 +0200

    improved javadoc: added UML diagrams

---
+++ mmm-client/mmm-client-ui/mmm-client-ui-widget/mmm-client-ui-widget-api/src/main/java/net/sf/mmm/client/ui/api/widget/field/UiWidgetCheckboxesField.java
@@ -0,0 +1,34 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+package net.sf.mmm.client.ui.api.widget.field;
+
+import java.util.Set;
+
+import net.sf.mmm.client.ui.api.widget.UiWidgetNative;
+import net.sf.mmm.client.ui.api.widget.UiWidgetOptionsBase;
+
+/**
+ * This is the interface for a {@link UiWidgetOptionsField options field widget} that represents a a set of
+ * <em>radio buttons</em>.<br/>
+ * <b>ATTENTION:</b><br/>
+ * You should only use this type of {@link UiWidgetOptionsField options field widgets} if you have a very limited number
+ * of {@link #getOptions() options} (less than seven). Otherwise use {@link UiWidgetComboboxField} instead.<br/>
+ * Here you can see an example (with the {@link #setOptions(java.util.List) options} set to {"single", "married",
+ * "divorced"}):
+ * 
+ * <pre>
+ * <span>
+ * <input type="radio">single</input><input type="radio">married</input><input type="radio">divorced</input>
+ * </span>
+ * </pre>
+ * 
+ * @author Joerg Hohwiller (hohwille at users.sourceforge.net)
+ * @since 1.0.0
+ * @param <VALUE> is the generic type of the {@link #getValue() value}.
+ */
+public interface UiWidgetCheckboxesField<VALUE> extends UiWidgetField<Set<VALUE>>, UiWidgetOptionsBase<VALUE>,
+    UiWidgetNative {
+
+  // nothing to add
+
+}
--- mmm-client/mmm-client-ui/mmm-client-ui-widget/mmm-client-ui-widget-api/src/main/java/net/sf/mmm/client/ui/api/widget/package-info.java
+++ mmm-client/mmm-client-ui/mmm-client-ui-widget/mmm-client-ui-widget-api/src/main/java/net/sf/mmm/client/ui/api/widget/package-info.java
@@ -9,8 +9,8 @@
  * {@link net.sf.mmm.client.ui.api.widget.factory.UiWidgetFactoryNative#create(Class) create} arbitrary
  * {@link net.sf.mmm.client.ui.api.widget.UiWidget}s. Use {@link net.sf.mmm.client.ui.api.UiContext}
  * to get access.<br/>
- * <img src="{@docRoot}/doc-files/widgets.png"/>
  * Please consult {@link net.sf.mmm.client.ui.api Client UI API} for an introduction.
+ * <img src="doc-files/Widgets.png"/>
  */
 package net.sf.mmm.client.ui.api.widget;
 
