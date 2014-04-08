96934b398e7a45ce1659243e328a9cb6ce6543c9
Joerg Hohwiller <joerg@j-hohwiller.de>
2014-04-09 00:28:38 +0200

    #96: moved multi-media-manager stuff to new repo (including obsolete or intermediate code)

--- mmm-data/mmm-data-api/src/main/java/net/sf/mmm/data/api/repository/package-info.java
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0 */
-/**
- * Provides the API for the data repository.
- * <a name="documentation"/><h2>Data API Repository</h2> 
- * This package contains the API for the data repository.
- */
-package net.sf.mmm.data.api.repository;
-
--- mmm-data/mmm-data-api/src/main/java/net/sf/mmm/data/base/package-info.java
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0 */
-/**
- * Provides the base implementation of the content API.
- * <a name="documentation"/><h2>Content Base</h2> 
- * This package contains the base implementation of the {@link net.sf.mmm.data.api content API}.
- */
-package net.sf.mmm.data.base;
-
