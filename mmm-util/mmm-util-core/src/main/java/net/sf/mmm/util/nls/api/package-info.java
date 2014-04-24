70648e26a4e7528eae2a0589add7f2231151e316
Joerg Hohwiller <joerg@j-hohwiller.de>
2014-04-24 23:41:30 +0200

    improved JavaDoc

--- mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/nls/api/package-info.java
+++ mmm-util/mmm-util-core/src/main/java/net/sf/mmm/util/nls/api/package-info.java
@@ -28,32 +28,38 @@
  * up with a cryptic message he can NOT understand (e.g. as illustrated by the
  * screenshot).<br>
  * On the other hand you need to know the locale of the receiver to do the l10n when creating the message or exception
- * with the proper text. This may lead to sick design such as static hacks.
+ * with the proper text. This may lead to sick design such as static hacks. Also if you have to translate the text at
+ * the creation of the message every receiver has to live with this language. Especially for logging this is a big problem.
+ * An operator will be lost in space if he gets such logfiles:
+ * <pre style='font-family: "Courier New", Courier, monospace'>
+ * [2000-01-31 23:59:00,000][ERROR][n.s.m.u.n.a.MasterService] The given value (256) has to be in the range from 0 to 100.
+ * [2000-01-31 23:59:01,000][WARN ][n.s.m.u.n.a.MasterService] Der Benutzername oder das Passwort sind ungültig.
+ * [2000-01-31 23:59:02,000][ERROR][n.s.m.u.n.a.MasterService] 文件不存在。
+ * [2000-01-31 23:59:03,000][FATAL][n.s.m.u.n.a.MasterService] ข้อผิดพลาดที่ไม่คาดคิดได้เกิดขึ้น
+ * </pre>
  * </p>
  * <h3>The Solution</h3>
  * The solution is quite simple:<br>
  * We simply bundle the message in default language together with the separated
- * locale independent arguments in one container object that is called
+ * dynamic arguments in one container object that is called
  * <code>{@link net.sf.mmm.util.nls.api.NlsMessage}</code>.
  * For exceptions there is additional support via <code>{@link net.sf.mmm.util.nls.api.NlsException}</code> and
  * <code>{@link net.sf.mmm.util.nls.api.NlsRuntimeException}</code>.
  * Here is an example to clarify the idea of {@link net.sf.mmm.util.nls.api.NlsMessage}:
- * The i18n message is "Hi {name}! How are you?" and the language independent argument is the users name e.g. "Lilli".
+ * The i18n message is "Hi {name}! How are you?" and the dynamic argument is the users name e.g. "Lilli".
  * Now if we store these informations together we have all we need. To get the localized message we simply translate the
  * i18n message to the proper language and then fill in the arguments. If we can NOT translate we always have the
  * message in default language which is "Hi Lilli! How are you?".<br>
  * But how do we translate the i18n message without artificial intelligence?
- * The answer is quite easy, but we offer two different approaches. These
- * are described in the following sections:
- * <h4>Solution 1: {@link net.sf.mmm.util.nls.api.NlsBundle}</h4>
+ * The answer is quite easy:
+ * <h4>Recommended Approach: {@link net.sf.mmm.util.nls.api.NlsBundle}</h4>
  * The recommended approach is to create an interface derived from {@link net.sf.mmm.util.nls.api.NlsBundle}.
  * For each message you define a method that takes the arguments to fill in and returns an
  * {@link net.sf.mmm.util.nls.api.NlsMessage}. Via annotations you provide the default message for each method.
  * <pre>
  * package foo.bar;
  *
- * &#64;{@link net.sf.mmm.util.nls.api.NlsBundleLocation}(bundleName = "NlsBundleFooBar")
- * public class NlsBundleFooBarRoot extends {@link net.sf.mmm.util.nls.api.NlsMessage} {
+ * public class NlsBundleFooBarRoot extends {@link net.sf.mmm.util.nls.api.NlsBundle} {
  *
  *   &#64;{@link net.sf.mmm.util.nls.api.NlsBundleMessage}("Hi {name}! How are you?")
  *   {@link net.sf.mmm.util.nls.api.NlsMessage} messageSayHi(&#64;{@link javax.inject.Named}("name") String name);
@@ -91,6 +97,10 @@
  * messageSayHi = Hallo {name}! Wie geht es Dir?
  * errorLoginInUse = Es tut uns leid. Das Login "{login}" ist bereits vergeben. Bitte wählen Sie ein anderes Login.
  * </pre>
+ * Unlike the Java defaults, here resource bundles are read in UTF-8 encoding and allow to use named parameters as
+ * alternative to indexes what makes it easier for localizers. There are even more advanced features such as recursive
+ * translation of arguments and choice format type. See {@link net.sf.mmm.util.nls.api.NlsMessage} for further details.
+ * Also our solution supports specific environments such as GWT (google web toolkit) what makes it very interoperable.
  *
  * In order to support you with creating and maintaining the localized
  * properties, this solution also comes with the
@@ -99,7 +109,7 @@
  * The advantage is that also the bundle name and key are available in the {@link net.sf.mmm.util.nls.api.NlsMessage}
  * and that this approach is GWT compatible when using <code>mmm-util-gwt</code>. However, there is still our legacy
  * approach.
- * <h4>Solution 2: {@link net.sf.mmm.util.nls.base.AbstractResourceBundle}</h4>
+ * <h4>Alternate Approach (legacy): {@link net.sf.mmm.util.nls.base.AbstractResourceBundle}</h4>
  * Simply create a subclass of {@link net.sf.mmm.util.nls.base.AbstractResourceBundle} that declares public string
  * constants:
  * <pre>
@@ -136,13 +146,7 @@
  * <code>META-INF/net.sf.mmm/nls-bundles</code> with the fully qualified name
  * of your bundle-class (foo.bar.FooBarResourceBundle) as content.
  * <br/>
- * For localization you can create property files with the translations of your
- * NLS-bundle. E.g. <code>foo/bar/FooBarResourceBundle_de.properties</code> with
- * this content:
- * <pre>
- * MSG_SAY_HI = Hallo {name}! Wie geht es Dir?
- * ERR_LOGIN_IN_USE = Es tut uns leid. Das Login "{login}" ist bereits vergeben. Bitte wählen Sie ein anderes Login.
- * </pre>
+ * For localization you can create property files as described above in the recommended approach.
  *
  * In order to support you with creating and maintaining the localized
  * properties, this solution also comes with the
@@ -150,7 +154,8 @@
  * <h3>Conclusion</h3>
  * As we have seen the NLS provided here makes it very easy for developers to write and maintain internationalized code.
  * While messages are created throughout the code they only need to be localized for the end-user in the client and at
- * service-endpoints. Only at these places you need to figure out the users locale.
+ * service-endpoints. Only at these places you need to figure out the users locale (see
+ * {@link net.sf.mmm.util.session.base.UserSessionProvider}).
  * <ul>
  *   <li>The {@link net.sf.mmm.util.nls.api.NlsMessage} allows to store an internationalized message together with
  *   actual arguments to fill in.</li>
@@ -159,6 +164,8 @@
  *   plural forms. See {@link net.sf.mmm.util.nls.api.NlsMessage} for advanced examples.</li>
  *   <li>In addition to numbered arguments that have been deprecated we also support named arguments. This makes
  *   maintenance of the messages a lot easier. Your localizers will love you for choosing this solution.</li>
+ *   <li>Resource bundle properties are read in UTF-8 encoding making it easier for localizers as they do not have to
+ *   escape characters to unicode number sequences.</li>
  *   <li>The localization (translation to native language) is easily performed by
  *   {@link net.sf.mmm.util.nls.api.NlsMessage#getLocalizedMessage(java.util.Locale)}.</li>
  *   <li>For exceptions there is additional support via {@link net.sf.mmm.util.nls.api.NlsThrowable}.</li>
