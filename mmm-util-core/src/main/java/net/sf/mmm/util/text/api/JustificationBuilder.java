747f45ec07199595e2beda7f918a678a2d56d37b
Joerg Hohwiller <hohwille@users.sourceforge.net>
2016-04-26 02:12:09 +0200

    #178: removed CDI_NAME

--- mmm-util-core/src/main/java/net/sf/mmm/util/text/api/JustificationBuilder.java
+++ mmm-util-core/src/main/java/net/sf/mmm/util/text/api/JustificationBuilder.java
@@ -5,8 +5,8 @@ package net.sf.mmm.util.text.api;
 import net.sf.mmm.util.component.api.ComponentSpecification;
 
 /**
- * This is the interface used to {@link #build(String) build} a {@link Justification} from a {@link String}.
- * It therefore expects the following format:<br>
+ * This is the interface used to {@link #build(String) build} a {@link Justification} from a {@link String}. It
+ * therefore expects the following format:<br>
  * <code>&lt;filler&gt;&lt;alignment&gt;&lt;width&gt;[&lt;mode&gt;]</code><br>
  * The segments have the following meaning:
  * <table border="1">
@@ -28,15 +28,15 @@ import net.sf.mmm.util.component.api.ComponentSpecification;
  * <tr>
  * <td><code>&lt;with&gt;</code></td>
  * <td><code>[0-9]+</code></td>
- * <td>if the length of the string to {@link Justification#justify(CharSequence, Appendable) justify} is less
- * than this width, the string will be expanded using the filler according to the alignment.</td>
+ * <td>if the length of the string to {@link Justification#justify(CharSequence, Appendable) justify} is less than this
+ * width, the string will be expanded using the filler according to the alignment.</td>
  * </tr>
  * <tr>
  * <td><code>&lt;mode&gt;</code></td>
  * <td><code>[|]</code></td>
- * <td>if the mode is truncate(|) then the string will be truncated if its length is greater than &lt;with&gt;
- * so the result will always have the length of &lt;with&gt;. Please note that truncate can remove valuable
- * information or cause wrong results (e.g. "10000" with a justification of " +3|" will result in "100").</td>
+ * <td>if the mode is truncate(|) then the string will be truncated if its length is greater than &lt;with&gt; so the
+ * result will always have the length of &lt;with&gt;. Please note that truncate can remove valuable information or
+ * cause wrong results (e.g. "10000" with a justification of " +3|" will result in "100").</td>
  * </tr>
  * </table>
  *
@@ -79,9 +79,6 @@ import net.sf.mmm.util.component.api.ComponentSpecification;
  */
 @ComponentSpecification
 public interface JustificationBuilder {
-
-  /** The {@link net.sf.mmm.util.component.api.Cdi#CDI_NAME CDI name}. */
-  String CDI_NAME = "net.sf.mmm.util.text.api.JustificationBuilder";
 
   /**
    * This method parses the given <code>format</code> as {@link Justification}.
--- mmm-util-core/src/main/java/net/sf/mmm/util/text/base/JustificationBuilderImpl.java
+++ mmm-util-core/src/main/java/net/sf/mmm/util/text/base/JustificationBuilderImpl.java
@@ -2,21 +2,16 @@
  * http://www.apache.org/licenses/LICENSE-2.0 */
 package net.sf.mmm.util.text.base;
 
-import javax.inject.Named;
-import javax.inject.Singleton;
-
 import net.sf.mmm.util.component.base.AbstractComponent;
 import net.sf.mmm.util.text.api.Justification;
 import net.sf.mmm.util.text.api.JustificationBuilder;
 
 /**
  * This is the implementation of {@link JustificationBuilder}.
- * 
+ *
  * @author Joerg Hohwiller (hohwille at users.sourceforge.net)
  * @since 1.1.1
  */
-@Singleton
-@Named(JustificationBuilder.CDI_NAME)
 public class JustificationBuilderImpl extends AbstractComponent implements JustificationBuilder {
 
   /** @see #getInstance() */
@@ -34,7 +29,7 @@ public class JustificationBuilderImpl extends AbstractComponent implements Justi
    * This method gets the singleton instance of this {@link JustificationBuilder}. <br>
    * <b>ATTENTION:</b><br>
    * Please read {@link net.sf.mmm.util.component.api.Cdi#GET_INSTANCE} before using.
-   * 
+   *
    * @return the singleton instance.
    */
   public static JustificationBuilder getInstance() {
