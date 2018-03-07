cd2741b43b7bd9efa83dd79aede1be0c3ca35571
Joerg Hohwiller <hohwille@users.sourceforge.net>
2018-03-07 21:43:51 +0100

    moved http to own repository

--- http/src/main/java/net/sf/mmm/util/http/api/entity/package-info.java
+++
@@ -1,6 +0,0 @@
-/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0 */
-/**
- * TODO: this class ...
- */
-package net.sf.mmm.util.http.api.entity;
--- http/src/main/java/net/sf/mmm/util/http/api/webdav/package-info.java
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0 */
-/**
- * TODO: this class ...
- *
- * @author hohwille
- * @since 8.5.0
- */
-package net.sf.mmm.util.http.api.webdav;
