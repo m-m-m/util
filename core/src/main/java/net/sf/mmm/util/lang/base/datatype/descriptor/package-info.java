942564e2a081fb220403874ad31a469ed491fcd1
Joerg Hohwiller <hohwille@users.sourceforge.net>
2018-03-07 13:31:09 +0100

    #248: deleted EnumDefinition stuff as well as DatatypeDescriptor stuff

--- core/src/main/java/net/sf/mmm/util/lang/base/datatype/descriptor/package-info.java
+++
@@ -1,9 +0,0 @@
-/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0 */
-/**
- * Contains the basic implementation of the Util Lang API for datatype description. <a name="documentation"></a>
- * <h2>Util Lang Base Datatype Descriptor</h2> This package contains the implementations of
- * {@link net.sf.mmm.util.lang.api.DatatypeDescriptorManager}, {@link net.sf.mmm.util.lang.api.DatatypeDescriptor}, and
- * {@link net.sf.mmm.util.lang.api.DatatypeSegmentDescriptor}.
- */
-package net.sf.mmm.util.lang.base.datatype.descriptor;
